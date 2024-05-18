9fd69bf5df20698038591c2244122c72ef331eea
AlterNayte <47716887+AlterNayte@users.noreply.github.com>
2024-05-18 16:52:02 +0200

    initial commit

---
+++ src/react-app/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5125',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: '../../src/PriceSignal/wwwroot'
+  }
+})
